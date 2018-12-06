a702253b3c153263834501b3049c519f0cb51c86
kscript <1258@live.cn>
2018-12-06 09:41:38 +0800

    删除无关代码

--- src/core/index.js
+++ src/core/index.js
@@ -1,18 +1,6 @@
-import util from './util';
+
 import {AST} from './ast';
-import init from './init';
-export const Vue = function (option) {
-  return new Vue.fn.Vue(option);
-}
-Object.assign(Vue, util, init);
-Vue.fn = Vue.prototype = {
-  constructor: Vue,
-  Vue: function (option) {
-    Vue.initOption.call(this, option);
-    return this;
-  }
-};
-// console.log([AST]);
+
 window.AST = AST;
 let sent = `
 var a = "var a = '1'",
@@ -30,9 +18,3 @@ var g = 4;
 var h=5;
 `
 window.ast = new AST(sent);
-
-Vue.fn.Vue.prototype = Vue.prototype;
-
-export default {
-  Vue
-}
--- src/main.js
+++ src/main.js
@@ -1,13 +1,1 @@
-import {Vue} from "./core";
-console.log(new Vue({
-  el: '#app',
-  data: {
-    show: false,
-    index: 1
-  },
-  methods: {
-    init(){
-      console.log(this)
-    }
-  }
-}), [Vue])
+import "./core";
